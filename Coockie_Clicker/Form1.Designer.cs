ed5f206abaa688e7b6d8c3f24235c88525cac0d7
Nikolai_itstep <student@COMP-507-1-02>
2022-01-29 12:26:13 +0200

    add name

--- Coockie_Clicker/Form1.Designer.cs
+++ Coockie_Clicker/Form1.Designer.cs
@@ -31,7 +31,8 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Coockie Clicker";
+            
         }
 
         #endregion
