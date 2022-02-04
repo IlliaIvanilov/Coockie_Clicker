6bf0691d0a6bfafc441f22ce2a201f0883d40271
k.tsyvat <k.tsyvat@gmail.com>
2022-02-04 10:51:09 +0200

    -- add little cookie on click

--- Coockie_Clicker/Form1.Designer.cs
+++ Coockie_Clicker/Form1.Designer.cs
@@ -52,17 +52,26 @@ namespace Coockie_Clicker
             button_box.Location = new Point(70, 300);
             this.button_box.Click += Button_Click;
 
-            normalcookie.BackgroundImage = Image.FromFile("img.png");
-            normalcookie.BackColor = Color.Transparent;
-            normalcookie.Size = new Size(x_cookie, y_cookie);
-            normalcookie.Location = new Point(65, 285);
-            normalcookie.SizeMode = PictureBoxSizeMode.Normal;
+            //normalcookie.BackgroundImage = Image.FromFile("img.png");
+            //normalcookie.BackColor = Color.Transparent;
+            //normalcookie.Size = new Size(x_cookie, y_cookie);
+            //normalcookie.Location = new Point(65, 285);
+            //normalcookie.SizeMode = PictureBoxSizeMode.Normal;
 
-            bigCookie.BackgroundImage = Image.FromFile("imgonline-com-ua-Resize-vFhzi5I0dmUifgY.png");
-            bigCookie.BackColor = Color.Transparent;
-            bigCookie.Size = new Size(x_cookie + 15, y_cookie + 15);
-            bigCookie.Location = new Point(65, 285);
-            bigCookie.SizeMode = PictureBoxSizeMode.Normal;
+            //bigCookie.BackgroundImage = Image.FromFile("imgonline-com-ua-Resize-vFhzi5I0dmUifgY.png");
+            //bigCookie.BackColor = Color.Transparent;
+            //bigCookie.Size = new Size(x_cookie + 15, y_cookie + 15);
+            //bigCookie.Location = new Point(65, 285);
+            //bigCookie.SizeMode = PictureBoxSizeMode.Normal;
+
+            littlecookie.BackgroundImage = Image.FromFile("imgonline-com-ua-resize-ve4lsmyqfsvk4s.png");
+            littlecookie.BackColor = Color.Transparent;
+            littlecookie.Size = new Size(35, 35);
+            littlecookie.Location = new Point(30, 300);
+            littlecookie.SizeMode = PictureBoxSizeMode.Normal;
+            littlecookie.BringToFront();
+
+            // this.Controls.Add(littlecookie);
 
             label_cockie.Size = new Size(300, 200);
             label_cockie.Location = new Point(45, 100);
@@ -70,6 +79,7 @@ namespace Coockie_Clicker
             label_cockie.TextAlign = ContentAlignment.MiddleCenter;
             label_cockie.Font = new Font("Times New Roman", 20, FontStyle.Bold);
             label_cockie.BackColor = Color.Transparent;
+            
             this.Controls.Add(label_cockie);
 
             this.Controls.Add(button_box);
@@ -86,11 +96,36 @@ namespace Coockie_Clicker
         Label label_cockie = new Label();
         Label label_setNull = new Label();
         Timer timer = new Timer();
-
         private void Button_Click(object sender, EventArgs e)
         {
+            // this.Cursor = new Cursor(Cursor.Current.Handle);
+            // var rand = new Random();
             coockie_click++;
             label_cockie.Text = string.Format("COOKIE COUNT \n{0}", coockie_click.ToString());
+            for(int i = 0; i < this.Controls.Count; i++)
+            {
+                littlecookie.Location = new Point(((System.Drawing.Size)Cursor.Position));
+                this.button_box.SendToBack();
+                this.littlecookie.BringToFront();
+                this.littlecookie.BringToFront();
+                this.Controls.Add(littlecookie);
+            }
+
+
+
+           // if (button_box.Size != new Size(265, 265))
+            //{
+            //    button_box.Size = new Size(265, 265);
+            //    button_box.SizeMode = PictureBoxSizeMode.Zoom;
+           // }
+//
+           // else
+            //{
+            //    button_box.Size = new Size(250, 250);
+            //    button_box.SizeMode = PictureBoxSizeMode.Zoom;
+            // }
+
+
             // this.Text = $"{coockie_click} cookies";
         }
         private void Form2_FormClosing(object sender, FormClosingEventArgs e)
